5db39bbee91bcaa622fd533c836f6d6ab9f92e69
Hemanth Kumar Siddraju <116480340+HimSiddd@users.noreply.github.com>
2024-03-20 21:37:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # loading the saved models
 
-diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open(f'{working_dir}/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open(f'{working_dir}/heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open(f'{working_dir}/saved_models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open(f'{working_dir}/parkinsons_model.sav', 'rb'))
 
 # sidebar for navigation
 with st.sidebar:
